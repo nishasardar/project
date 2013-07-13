0d13376ee8e2997bc8ddfcc297208183644b4b23
Nisha S <nisha@salesforceresults.co.nz>
2013-07-13 15:48:20 +0530

    dded

---
+++ Liscence.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	
+	
+	public function down()
+	{
+		Schema::drop('Users');
+	}
+
+}
